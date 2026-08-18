23ebc7a23357addb3ec22ef960eac5b0a37d597d
sovereignuniversity20-art <sovereign.university20@gmail.com>
2026-08-18 10:22:15 -0500

    updated with correct file

--- advanced-sql-queries-S-H.sql
+++ advanced-sql-queries-S-H.sql
@@ -1,1 +1,36 @@
-SELECT * FROM online_store.customers;
+select * from customers;
+select * from orders;
+SELECT customer_id, AVG(total_amount) AS
+average_spent
+FROM orders
+GROUP BY customer_id;
+SELECT order_date, SUM(total_amount) AS
+daily_total
+FROM orders
+GROUP BY order_date;
+SELECT order_date, SUM(total_amount) AS
+daily_total
+FROM orders
+WHERE total_amount > 150
+GROUP BY order_date;
+SELECT id, first_name, last_name 
+FROM customers
+WHERE last_name = "Smith"
+GROUP BY id;
+SELECT orders.id, customers.first_name, 
+customers.last_name, orders.order_date,
+orders.total_amount
+FROM orders
+INNER JOIN customers ON orders.customer_id
+= customers.id;
+SELECT orders.id, customers.first_name, 
+customers.last_name, orders.order_date,
+orders.total_amount
+FROM orders
+RIGHT JOIN customers ON orders.customer_id
+= customers.id;
+SELECT id, order_date, total_amount
+FROM orders
+WHERE total_amount <= (SELECT
+AVG(total_amount)FROM orders);
+
